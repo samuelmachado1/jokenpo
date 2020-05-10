138c055c94ecbc5dfb9af75d273a677c45ab59bf
Samuel Estrella <samuelmachado1@gmail.com>
2020-05-09 22:09:54 -0300

    Redefining the prints and finalizing the project implementation.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from random import randint
+from time import sleep
+
 itens = ('Pedra', 'Papel', 'Tesoura')
 results = ('EMPATE!', 'JOGADOR VENCEU!',' COMPUTADOR VENCEU!', 'JOGADA INVÁLIDA!')
+
 computador = randint(0, 2)
 print('''Suas opções:
-[ 0 ] PEDRA
-[ 1 ] PAPEL
-[ 2 ] TESOURA
+    [ 0 ] PEDRA
+    [ 1 ] PAPEL
+    [ 2 ] TESOURA
  ''')
+
 jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('JO!')
 print('-=' * 11)
 print('Jogador jogou {}'.format(itens[jogador])),
 print('Computador jogou {}'.format(itens[computador]))
