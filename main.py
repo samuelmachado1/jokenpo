d1a47ed9d0cdc9cb2eea8187a4f3be0c064c5d21
Samuel Estrella <samuelmachado1@gmail.com>
2020-05-09 21:38:24 -0300

    Starting project and its base structure.

---
+++ main.py
@@ -0,0 +1,10 @@
+from random import randint
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA
+ ''')
+
+jogador = int(input('Qual é a sua jogada? '))
