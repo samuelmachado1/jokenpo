4abf084c0916c8ec1c91b51221c5597a1b7d4d63
Samuel Estrella <samuelmachado1@gmail.com>
2020-05-09 21:51:02 -0300

    Implementing game logic and print rules

--- main.py
+++ main.py
@@ -6,5 +6,35 @@ print('''Suas opções:
 [ 1 ] PAPEL
 [ 2 ] TESOURA
  ''')
-
 jogador = int(input('Qual é a sua jogada? '))
+print('-=' * 11)
+print('Jogador jogou {}'.format(itens[jogador])),
+print('Computador jogou {}'.format(itens[computador]))
+print('-=' * 11)
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE!')
+    elif jogador == 1:
+        print('JOGADOR VENCEU!')
+    elif jogador == 2:
+        print('COMPUTADOR VENCEU!')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCEU!')
+    elif jogador == 1:
+        print('EMPATE!')
+    elif jogador == 2:
+         print('JOGADOR VENCEU!')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 2:
+    if jogador == 0:
+        print('JOGADOR VENCEU!')
+    elif jogador == 1:
+        print('COMPUTADOR VENCEU!')
+    elif jogador == 2:
+        print('EMPATE!')
+    else:
+        print('JOGADA INVÁLIDA!')
